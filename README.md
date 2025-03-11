2ba2441d4a93535960aec34fcaa6434cf016c132
Anthony Simonov <asimonov@engee.cloud>
2025-03-12 01:13:47 +0300

    terminal that works

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Connector
 
 Programm for connecting to terminal on 'server' terminal from 'client' via LAN
+
+### Quick start
+1. Create virtual enviroment (using virtualenv)
+2. Activate venv
+3. Download dependencies `pip install -r ./requirements.txt`
+*4. Optional: download dev dependencies `pip install -r ./requirements.txt`*
+
+#### Formatting
+Use `make format` to prettify code
--- src/terminal.py
+++ src/terminal.py
@@ -1,105 +1,147 @@
 import asyncio
+import subprocess
 
-import sys
-import os
-from subprocess import PIPE, STDOUT, Popen
-from loguru import logger
-from enum import Enum
+from prompt_toolkit import PromptSession
+from prompt_toolkit.patch_stdout import patch_stdout
 
 
-class States(Enum):
-    IDLE = 1
-    RUNNING = 2
+class InteractiveShell:
+    """
+    Класс предназначенный для работы с терминалов в интерактивном режиме
 
+    Интерфейс предусматривает ввод входных данных через соответствующую очередь
+        `input_queue`
+    Выходные данные попадают в очередь
+        `output_queue`
+    """
 
-class LocalShell:
-    def __init__(self) -> None:
-        self.__writer: asyncio.Task | None = None
-        self.__reader: asyncio.Task | None = None
-        self.__process: Popen | None = None
-        self.state = States.IDLE
+    def __init__(self, terminal_name: str = "bash"):
+        self.process = None
+        self.terminal_name = terminal_name
 
-    async def _writer_process(self, output_queue: asyncio.Queue) -> None:
-        logger.info("writer active")
-        while True:
-            data = self.__process.stdout.read(1).decode("utf-8")
-            logger.info(data)
-            if not data:
-                await asyncio.sleep(0.01)
-                continue
-            await output_queue.put_nowait(data)
+    async def start(self, input_queue: asyncio.Queue, output_queue: asyncio.Queue):
+        """
+        Запуск терминала в интерактивном режиме с прокидыванием
+            stdin, stdout, stderr через туннели с их обработкой
 
-    def _write_to_process(self, process: Popen, message: str):
-        process.stdin.write(message.encode())
-        process.stdin.flush()
-
-    async def _reader_process(self, input_queue: asyncio.Queue) -> None:
-        logger.info("reader active")
-        while True:
-            data = await input_queue.get()
-            logger.info(data)
-            if not data or len(data) > 1:
-                # logger.warning(f"Incorrect queue arg {data=}")
-                await asyncio.sleep(0.01)
-                continue
-            self._write_to_process(self.__process, data)
-
-    async def run(
-        self, input_queue: asyncio.Queue, output_queue: asyncio.Queue
-    ) -> None:
-        if self.__writer or self.__reader:
-            raise ValueError("Process is running")
-
-        env = os.environ.copy()
-        self.__process = Popen(
-            "/bin/bash",
-            stdin=PIPE,
-            stdout=PIPE,
-            stderr=STDOUT,
-            shell=True,
-            env=env,
+        stdin пишется из данных, поступаемых через очередь `input_queue`
+        stdout, stderr записываются в очередь `output_queue`
+        """
+        self.process = await asyncio.create_subprocess_exec(
+            f"/bin/{self.terminal_name}",
+            "-i",
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            universal_newlines=False,
         )
-        self.__writer = asyncio.create_task(self._writer_process(output_queue))
-        self.__reader = asyncio.create_task(self._reader_process(input_queue))
-        self.state = States.RUNNING
 
-    async def terminate(self) -> None:
-        if self.state != States.RUNNING:
-            return
-        self.__process.kill()
-        if self.__writer:
-            await self.__writer
-        if self.__reader:
-            await self.__reader
-        self.state = States.IDLE
+        self.stdout_task = asyncio.create_task(self.read_stdout(output_queue))
+        self.stderr_task = asyncio.create_task(self.read_stderr(output_queue))
+        self.input_task = asyncio.create_task(self.handle_input(input_queue))
+
+    async def shutdown(self) -> None:
+        """
+        Завершение терминала, а также всех сопроводительных процессов
+        """
+        await asyncio.gather(self.stdout_task, self.stderr_task)
+        self.input_task.cancel()
+        try:
+            await self.input_task
+        except asyncio.CancelledError:
+            pass
+
+        self.process.stdin.close()
+        await self.process.wait()
+
+    async def handle_input(self, input_queue: asyncio.Queue) -> None:
+        """
+        Метод для прокидывания входных данных из очереди в запущенный терминал
+        """
+        while True:
+            command = await input_queue.get()
+            if command.lower() == "exit":
+                print("Exiting shell.")
+                self.process.stdin.write("exit\n".encode())
+                await self.process.stdin.drain()
+                break
+            self.process.stdin.write((command + "\n").encode())
+            await self.process.stdin.drain()
+
+    async def read_stdout(self, output_queue: asyncio.Queue) -> None:
+        """
+        Метод для прокидывания выходных данных в очередь
+        """
+        while True:
+            chunk = await self.process.stdout.read(1024)
+            if not chunk:
+                output_queue.put_nowait(None)
+                break
+            output_queue.put_nowait(chunk.decode("utf-8"))
+
+    async def read_stderr(self, output_queue: asyncio.Queue) -> None:
+        """
+        Метод для прокидывания выходных данных ошибок в очередь
+        """
+        while True:
+            chunk = await self.process.stderr.read(1024)
+            if not chunk:
+                output_queue.put_nowait(None)
+                break
+            output_queue.put_nowait(f"Error: {chunk.decode('utf-8')}")
 
 
 class MockInteract:
-    async def inp(self, q: asyncio.Queue) -> None:
+    """
+    Класс для тестирования работы терминала через пользовательский ввод в stdin
+    """
+
+    def __init__(self):
+        self.session = PromptSession()
+
+    async def run(self, input_queue, output_queue):
+        self.input_reader = asyncio.create_task(self.read_input(input_queue))
+        self.output_reader = asyncio.create_task(self.handle_output(output_queue))
+
+    async def read_input(self, input_queue):
         while True:
-            s = input()
-            for letter in s:
-                await q.put_nowait(letter)
-                await asyncio.sleep(0.01)
+            with patch_stdout():
+                command = await self.session.prompt_async(">>> ")
+                await input_queue.put(command)
 
-    async def out(self, q: asyncio.Queue) -> None:
+    async def handle_output(self, output_queue):
         while True:
-            letter = await q.get_nowait()
-            await asyncio.sleep(0.01)
-            logger.info(letter)
+            output = await output_queue.get()
+            if output is None:
+                break
+            print(output, end="")
 
-    async def main(self):
-        shell = LocalShell()
-        inp_q = asyncio.Queue()
-        out_q = asyncio.Queue()
-        t1 = asyncio.create_task(self.inp(inp_q))
-        t2 = asyncio.create_task(self.out(out_q))
-        await shell.run(inp_q, out_q)
-        logger.info(f"{shell.state}")
-        await t1
-        await t2
-        await shell.terminate()
+    async def shutdown(self):
+        if not self.input_reader.done():
+            self.input_reader.cancel()
+        if not self.output_reader.done():
+            self.output_reader.cancel()
+        try:
+            await self.input_reader
+        except asyncio.CancelledError:
+            pass
+        try:
+            await self.output_reader
+        except asyncio.CancelledError:
+            pass
 
 
-mock = MockInteract()
-asyncio.run(mock.main())
+async def main():
+    input_queue = asyncio.Queue()
+    output_queue = asyncio.Queue()
+    shell = InteractiveShell(terminal_name="zsh")
+    interaction = MockInteract()
+
+    await interaction.run(input_queue, output_queue)
+    await shell.start_interactive_shell(input_queue, output_queue)
+    await shell.shutdown()
+    await interaction.shutdown()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
